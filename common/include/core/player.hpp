607dde7834e94cb6b651195ebfebd2b97f4fadbc
StackZ <47382115+SuperSaiyajinStackZ@users.noreply.github.com>
2021-01-02 22:44:33 +0100

    2020 -> 2020-2021.

--- common/include/core/player.hpp
+++ common/include/core/player.hpp
@@ -1,6 +1,6 @@
 /*
 	Diese Datei ist Teil von Ludo3DS.
-	Copyright (C) 2020 bei SuperSaiyajinStackZ.
+	Copyright (C) 2020-2021 bei SuperSaiyajinStackZ.
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
