5d4af231fcf34a5895bb9ed13c0729ae0c53534f
StackZ <47382115+SuperSaiyajinStackZ@users.noreply.github.com>
2021-01-09 18:58:21 +0100

    Neuer Splash!

--- 3ds/source/overlays/SplashOverlay.cpp
+++ 3ds/source/overlays/SplashOverlay.cpp
@@ -26,96 +26,86 @@
 
 #include "common.hpp"
 
-#define _SPLASH_X_LOGO_POS 170
-#define _SPLASH_LOGO_INIT_DELAY 50
-#define _SPLASH_WAIT_DELAY 200
-
-/*
-	Zeichne den Splash-Screen.
-
-	const int &logoPos: Die Position des 3DElf's Logo.
-	const int &fadeAlpha: Der Fade Alpha wert.
-	const uint32_t &year: Das aktulle Jahr.
-*/
-static void Draw(const int &logoPos, const int &fadeAlpha, const uint32_t &year) {
+static void Draw(const uint16_t xPos, const uint16_t yPos, const uint8_t diceRes, const bool shine, const uint8_t fade) {
 	Gui::clearTextBufs();
 	C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
 	C2D_TargetClear(Top, NO_COLOR);
 	C2D_TargetClear(Bottom, NO_COLOR);
+	Gui::ScreenDraw(Top);
 
-	GFX::DrawBaseTop();
-	Gui::Draw_Rect(0, 0, 400, 25, BAR_COLOR);
-	Gui::Draw_Rect(0, 215, 400, 25, BAR_COLOR);
+	Gui::Draw_Rect(0, 0, 400, 240, C2D_Color32(132, 60, 0, 255));
+	GFX::DrawField(75, 15);
+	GFX::Dice(diceRes, 300, 80);
 
-	Gui::DrawStringCentered(0, 1, 0.7f, TEXT_COLOR, Lang::get("STACKZ_PRESENTS"));
-	GFX::DrawSprite(sprites_stackZ_idx, 2, 75);
-	if (logoPos < 400) GFX::DrawSprite(sprites_Ludo3DS_idx, logoPos, 35);
-	Gui::DrawStringCentered(0, 217, 0.7f, TEXT_COLOR, "2020 - " + std::to_string(year), 390);
-	if (fadeAlpha > 0) Gui::Draw_Rect(0, 0, 400, 240, C2D_Color32(0, 0, 0, fadeAlpha));
+	GFX::DrawFigure(0, xPos, yPos);
+	GFX::DrawFigure(1, 133, 111);
+	GFX::DrawFigure(2, 171, 54);
+	GFX::DrawFigure(3, 247, 111);
+	if (shine) GFX::DrawPlayerSelector(0, xPos, yPos);
+
+	if (fade > 0) Gui::Draw_Rect(0, 0, 400, 240, C2D_Color32(0, 0, 0, fade));
 
 	GFX::DrawBaseBottom();
-	GFX::DrawSprite(sprites_universal_core_idx, 0, 26);
-	if (fadeAlpha > 0) Gui::Draw_Rect(0, 0, 320, 240, C2D_Color32(0, 0, 0, fadeAlpha));
-
+	if (!shine) GFX::DrawSprite(sprites_universal_core_idx, 0, 26);
+	else GFX::DrawSprite(sprites_Ludo3DS_idx, 63, 35);
+	if (fade > 0) Gui::Draw_Rect(0, 0, 320, 240, C2D_Color32(0, 0, 0, fade));
 	C3D_FrameEnd(0);
 }
 
-/*
-	Zeige den Splash-Screen.
-*/
 void Overlays::SplashOverlay() {
-	/* Initiale Werte. */
-	int delay = _SPLASH_WAIT_DELAY, logoPos = 402, swipeDelay = _SPLASH_LOGO_INIT_DELAY, fadeAlpha = 255;
-	bool doOut = false, swipedIn = false, doSwipe = false, fadeInSplash = true;
+	bool doOut = false, firstX = true, shine = false, doDice = true;
 
-	time_t currentTime = time(NULL);
-	struct tm *currentTimeStruct = localtime(&currentTime);
-	const uint16_t year = 1900 + currentTimeStruct->tm_year; // uint16_t reicht aus, weil der 3DS nur bis 2050 geht.
+	uint8_t xTODO = 19, yTODO = 19 * 4, diceRes = 1, fade = 252;
+	uint16_t xPos = 190, yPos = 206, shineDelay = 100;
 
 	while(!doOut) {
 		/* Fade in effekt. */
-		if (fadeInSplash) {
-			if (fadeAlpha > 0) {
-				fadeAlpha -= 2;
+		if (fade > 0) {
+			fade -= 6;
 
-				if (fadeAlpha <= 0) {
-					fadeAlpha = 0;
-					fadeInSplash = 0;
-				}
-			}
+			if (fade <= 0) fade = 0;
 		}
 
-		Draw(logoPos, fadeAlpha, year);
-
+		Draw(xPos, yPos, diceRes, shine, fade);
 		hidScanInput();
-
 		if (hidKeysDown()) doOut = true;
 
-		/* Als erstes die Initiale Overlay Verzögerung. */
-		if (!swipedIn) {
-			if (swipeDelay > 0) {
-				swipeDelay--;
 
-				if (swipeDelay == 0) doSwipe = true;
+		if (!fade && doDice) {
+			if (diceRes < 5) {
+				for (int i = 0; i < 15; i++) { gspWaitForVBlank(); };
+				diceRes++;
+
+				if (diceRes == 5) doDice = false;
 			}
 		}
 
-		/* Dann die Logo Animation. */
-		if (doSwipe) {
-			if (logoPos > _SPLASH_X_LOGO_POS) {
-				logoPos--;
+		if (!fade && !doDice && firstX) {
+			if (xTODO > 0) {
+				for (int i = 0; i < 1; i++) { gspWaitForVBlank(); };
+				xPos--;
+				xTODO--;
 
-				if (logoPos == _SPLASH_X_LOGO_POS) swipedIn = true;
+				if (xTODO == 0) firstX = false;
 			}
 		}
 
-		/* Dann die restliche Logik. */
-		if (swipedIn) {
-			if (delay > 0) {
-				delay--;
+		if (!fade && !doDice && !firstX) {
+			if (yTODO > 0) {
+				for (int i = 0; i < 1; i++) { gspWaitForVBlank(); };
+				yTODO--;
+				yPos--;
 
-				/* Falls Delay == 0, verlasse das Overlay. */
-				if (delay == 0) doOut = true;
+				if (yTODO == 0) shine = true;
+			}
+		}
+
+		if (shine) {
+			if (shineDelay > 0) {
+				for (int i = 0; i < 2; i++) { gspWaitForVBlank(); };
+				shineDelay--;
+
+				if (shineDelay == 0) doOut = true;
 			}
 		}
 	}
